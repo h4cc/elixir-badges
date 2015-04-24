fb3d103d24d08822a70974fa284223ef33034ba7
Julius Beckmann <github@h4cc.de>
2015-04-24 14:14:56 +0200

    Made badge smaller. Added Cache-Control Header

--- web/controllers/badge_controller.ex
+++ web/controllers/badge_controller.ex
@@ -1,7 +1,7 @@
 defmodule ElixirBadges.BadgeController do
   use ElixirBadges.Web, :controller
 
-  @font_size 14
+  @font_size 12
   @font_family "DejaVu Sans,Verdana,Geneva,sans-serif"
   @width_per_char 7.4
 
@@ -11,6 +11,7 @@ defmodule ElixirBadges.BadgeController do
     package = Hex.get_package(name)
     conn
       |> put_resp_content_type("image/svg+xml")
+      |> put_resp_header("cache-control", "max-age=60")
       |> send_resp(200, licenses_badge(package.licenses))
   end
 
@@ -18,7 +19,7 @@ defmodule ElixirBadges.BadgeController do
   defp licenses_badge(licenses) when is_list licenses do
     content = Enum.join(licenses, ", ")
 
-    width_s = to_string(width = (18 + (String.length(content) * @width_per_char)))
+    width_s = to_string(width = (14 + (String.length(content) * @width_per_char)))
 
     "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" <> width_s <> "\" height=\"20\">
 
@@ -32,13 +33,13 @@ defmodule ElixirBadges.BadgeController do
       </mask>
     
       <g mask=\"url(#round)\">
-        <rect x=\"0\" width=\"" <> width_s <> "\" height=\"20\" fill=\"#ccc\"/>
+        <rect x=\"0\" width=\"" <> width_s <> "\" height=\"20\" fill=\"#ddd\"/>
         <rect width=\"" <> width_s <> "\" height=\"20\" fill=\"url(#smooth)\"/>
       </g>
     
       <g fill=\"#fff\" text-anchor=\"middle\" font-family=\""<>@font_family<>"\" font-size=\"" <> to_string(@font_size) <> "\">
-        <text x=\"" <> to_string(width / 2 + 1) <> "\" y=\"16\" fill=\"#010101\" fill-opacity=\".3\">"<>content<>"</text>
-        <text x=\"" <> to_string(width / 2 + 1) <> "\" y=\"15\">"<>content<>"</text>
+        <text x=\"" <> to_string(width / 2) <> "\" y=\"16\" fill=\"#010101\" fill-opacity=\".3\">"<>content<>"</text>
+        <text x=\"" <> to_string(width / 2) <> "\" y=\"15\">"<>content<>"</text>
       </g>
     </svg>"
   end
